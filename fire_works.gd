b607f2ee58eff850de7139e59d84004b2d5b8e80
Anuj Dubey <dubey2175@gmail.com>
2025-08-14 22:32:55 +0530

    Fire works Explosion added

---
+++ fire_works.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var rocket = $RocketTrail
+@onready var explosion = $Explosion
+@onready var timer = $Timer
+
+func _ready():
+	start_firework()
+
+func start_firework():
+	# Start the rocket
+	rocket.emitting = true
+	# Set timer to trigger explosion (adjust to match rocket travel time)
+	timer.wait_time = 1.0
+	timer.start()
+
+func _on_Timer_timeout():
+	# Stop rocket trail
+	rocket.emitting = false
+	# Trigger explosion
+	explosion.emitting = true
